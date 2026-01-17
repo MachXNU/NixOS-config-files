1cbced656d06f7b7d88ec591b7a29df01dcb1bcf
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-17 13:47:37 +0100

    Fixed git config to the new syntax

--- users/jb/home.nix
+++ users/jb/home.nix
@@ -16,11 +16,13 @@
 
   programs.git = {
     enable = true;
-    userName = "Jb";
-    userEmail = "42501418+MachXNU@users.noreply.github.com";
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+      user = {
+        name = "Jb";
+        email = "42501418+MachXNU@users.noreply.github.com";
+      };
     };
   };
 
