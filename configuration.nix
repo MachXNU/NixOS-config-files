11d562ed9664288a45d81354301e2e140dc089a3
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-19 20:02:40 +0100

    Better settings for OpenSSH

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,17 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "jb" ];
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
