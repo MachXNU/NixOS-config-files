80658ba71d7a9079adc89e1bc0ddd5c4e3cacd30
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-20 18:44:45 +0100

    Blue Mountains wallpaper set by swww (via systemd again)

--- home-manager/desktop/swww/default.nix
+++ home-manager/desktop/swww/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  wallpaper = ../Wallpapers/minimalist-blue-mountains_8001x4501.jpg; 
+in
 {
+  # swww daemon 
   systemd.user.services.swww = {
     Unit = {
       Description = "swww wallpaper daemon";
@@ -17,6 +21,31 @@
       Environment = [
         "XDG_RUNTIME_DIR=%t"
       ];
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  # swww client, set wallpaper
+  home.file."wallpapers/nord.png".source = wallpaper;
+
+  systemd.user.services.swww-set-wallpaper = {
+    Unit = {
+      Description = "Set wallpaper via swww";
+      After = [ "swww.service" ];
+      Requires = [ "swww.service" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.swww}/bin/swww img \
+          ${config.home.homeDirectory}/wallpapers/nord.png \
+          --transition-type grow \
+          --transition-duration 1
+      '';
     };
 
     Install = {
