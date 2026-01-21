caa8795816314d0b15c1e52bf40d4ae4f52576c2
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-21 19:34:37 +0100

    Added Hyprland misc config section

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -9,10 +9,12 @@
     enable = true;
     systemd.enable = true;
   };  
+
   imports = [
     ./binds.nix
     ./decoration.nix
     ./general.nix
     ./animations.nix
+    ./misc.nix
   ];
 }
---
+++ home-manager/desktop/hyprland/misc.nix
@@ -0,0 +1,13 @@
+_: {
+
+  wayland.windowManager.hyprland.settings = {
+    misc = {
+      force_default_wallpaper = 0;
+      disable_hyprland_logo   = true;
+    };
+
+    exec-once = [
+      "waybar"
+    ];
+  };
+}
