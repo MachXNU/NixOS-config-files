d3ba0533b52130ba3b616895763be9396d072de6
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-24 13:02:39 +0100

    Fix invisible muted waybar module + (re-)added the calendar on the date module

--- home-manager/desktop/waybar/modules.nix
+++ home-manager/desktop/waybar/modules.nix
@@ -4,12 +4,31 @@
   clock = {
     format = "{:%I:%M %p}";
     format-alt = "{:%a, %d. %b}";
+    tooltip-format = "<tt><small>{calendar}</small></tt>";
+    calendar = {
+      mode =       "year";
+      mode-mon-col = 3;
+      weeks-pos    = "right";
+      on-scroll    = 1;
+      format = {
+        months   = "<span color='#ffead3'><b>{}</b></span>";
+        days     = "<span color='#ecc6d9'><b>{}</b></span>";
+        weeks    = "<span color='#99ffdd'><b>W{}</b></span>";
+        weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+        today    = "<span color='#ff6699'><b><u>{}</u></b></span>";
+      };
+      actions = {
+        on-click-right = "mode";
+        on-scroll-up   = "shift_up";
+        on-scroll-down = "shift_down";
+      };
+    };
   };
 
   pulseaudio = {
     format = "{volume}% {icon}";
     format-bluetooth = "{volume}% {icon}";
-    format-muted = "";
+    format-muted = "";
     format-icons = {
       headphone = "";
       hands-free = "󰓃";
