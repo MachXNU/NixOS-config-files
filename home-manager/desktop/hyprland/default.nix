ae67ee66a7a37c59d9d535d92c9da8311fae588b
Jb <42501418+MachXNU@users.noreply.github.com>
2026-01-30 23:08:53 +0100

    Hyprland input, including trackpad

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -16,5 +16,6 @@
     ./general.nix
     ./animations.nix
     ./misc.nix
+    ./inputs.nix
   ];
 }
---
+++ home-manager/desktop/hyprland/inputs.nix
@@ -0,0 +1,27 @@
+_: {
+
+  wayland.windowManager.hyprland.settings = {
+    input = {
+      kb_layout = "us";
+      repeat_rate = 50;
+      repeat_delay = 300;
+
+      sensitivity = 0; #mouse sensitivity
+      numlock_by_default = true;
+      left_handed = false;
+      follow_mouse = true;
+      float_switch_override_focus = false;
+
+      touchpad = {
+        disable_while_typing = true;
+        natural_scroll = true;
+
+        # maps 1-2-3 fingers click to LMB, RMB and MMB
+        clickfinger_behavior = true;
+        middle_button_emulation = true;
+        tap-to-click = false;
+        drag_lock = true;
+      };
+    };
+  };
+}
