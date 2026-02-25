8060f7583fc7fc28d98d998850c7dc949220a135
Jb <42501418+MachXNU@users.noreply.github.com>
2026-02-25 19:30:38 +0100

    [NVF] WIP 2 (broken)

--- home-manager/cli/nvf.nix
+++ home-manager/cli/nvf.nix
@@ -6,7 +6,7 @@
         viAlias = true;
         vimAlias = true;
 
-#         options = {
+        options = {
 #           expandtab = true;
 #           shiftwidth = 2;
 #           softtabstop = 2;
@@ -16,17 +16,18 @@
 #           smartindent = true;
 #           showmatch = true;
 #           backspace = "indent,eol,start";
-#         };
+          shada = "!,'100,<50,s10,h";  
+        };
 
         lsp = {
           enable = true;
 
           formatOnSave = true;
-          lspkind.enable = false; # Adds icons to completion items (like funcitons, packages, structs, etc.)
+          lspkind.enable = true; # Adds icons to completion items (like funcitons, packages, structs, etc.)
           lightbulb.enable = true;
           lspsaga.enable = false; # Enhancements to LSP UI, more IDE look, worth trying later
           trouble.enable = true;  # Better display of warnings and errors
-          lspSignature.enable = true; # Show function signature while typing
+          lspSignature.enable = false; # Show function signature while typing, clashes with blink-cmp
           otter-nvim.enable = false;  # LSP support for files with different languages
           nvim-docs-view.enable = false;
           harper-ls.enable = true; # Grammar check running locally
@@ -61,11 +62,121 @@
         statusline = {
           lualine = {
             enable = true;
-            theme = "catppuccin";
+            theme = "auto";
           };
         };
 
-        syntaxHighlighting = true;
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+          transparent = false;
+        };
+
+        autocomplete.blink-cmp = {
+          enable = true;
+          friendly-snippets.enable = true;
+
+          mappings = {
+            next = "<C-n>";
+            previous = "<C-p>";
+            confirm = "<CR>";
+            complete = "<C-Space>";
+            close = "<C-e>";
+          };
+
+          setupOpts = {
+            fuzzy.implementation = "prefer_rust";
+            completion.documentation.auto_show = true;
+            keymap.preset = "default";
+          };
+        };
+
+        autopairs.nvim-autopairs.enable = true;
+
+        filetree = {
+          neo-tree = {
+            enable = true;
+          };
+        };
+
+        tabline = {
+          nvimBufferline.enable = true;
+        };
+
+        treesitter.context.enable = true;
+
+        binds = {
+          whichKey.enable = true;
+          cheatsheet.enable = true;
+        };
+
+        telescope.enable = true;
+
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+          gitsigns.codeActions.enable = false; # throws an annoying debug message
+          neogit.enable = false;
+        };
+
+        minimap = {
+          minimap-vim.enable = false;
+          codewindow.enable = true;  # might get annoying
+        };
+
+        dashboard = {
+          dashboard-nvim.enable = false;
+          alpha.enable = true;
+        };
+
+        notify = {
+          nvim-notify.enable = true;
+        };
+
+        projects = {
+          project-nvim.enable = true;  # really useful?
+        };
+
+        utility = {
+          ccc.enable = true;                  # color picker
+          vim-wakatime.enable = false;        # tracks coding time
+          diffview-nvim.enable = true;        # view git diff nicely
+          yanky-nvim.enable = true;           # enhanced yank/paste, history
+          icon-picker.enable = true;          # emoji selector    
+          surround.enable = true;             # wrap content in quotes, brackets, etc.
+          multicursors.enable = false;        # what the hell is even that?
+          undotree.enable = false;            # visualize undo history as a tree
+          nvim-biscuits.enable = true;        # show current fct/class inline at the top of a block
+          grug-far-nvim.enable = false;       # project-wide search and replace
+
+          motion = {
+            hop.enable = true;                # jump anywhere on screen using hint characters
+            leap.enable = true;               # fast 2 characters jump motion
+            precognition.enable = true;       # shows suggested motions (teaches VIM navigation)
+          };
+
+          images = {
+            image-nvim.enable = true;         # render images inside Neovim buffer
+            img-clip.enable = true;           # paste images from clipboard into markdown/org/etc.
+          };
+        };
+
+        ui = {
+          borders.enable = true;
+          noice.enable = true;
+          colorizer.enable = true;
+          modes-nvim.enable = false; # the theme looks terrible with catppuccin
+          illuminate.enable = true;
+          breadcrumbs = {
+            enable = true;
+            navbuddy.enable = true;
+          };
+        };
+
+        comments = {
+          comment-nvim.enable = true;
+        };
       };
     };
   };
