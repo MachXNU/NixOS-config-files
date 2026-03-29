435412d2ef6ac73998f5e58da8c67e7ffdfdff9d
MachXNU <42501418+MachXNU@users.noreply.github.com>
2026-03-29 12:49:19 +0200

    systemd security profile for ddns-updater, score of 1.1

--- modules/ddns-updater.nix
+++ modules/ddns-updater.nix
@@ -15,15 +15,50 @@
     Environment = [
       "GODEBUG=netdns=go"
     ];
+    AmbientCapabilities = "";
+    CapabilityBoundingSet = "";
+    DevicePolicy = "closed"; # fails at stdin if "strict"
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    ProcSubset = "pid";
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    ProtectProc = "noaccess";
+    ProtectSystem = "strict";
+    PrivateDevices = true;
+    PrivateTmp = true;
+    PrivateUsers = true;
+    RemoveIPC = true;
+    RestrictAddressFamilies = [
+      "AF_INET"
+      "AF_INET6"
+      "AF_UNIX"
+    ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    SystemCallArchitectures = "native";
+    SystemCallFilter = [
+      "@system-service"
+      "~@privileged"
+      "~@resources"
+    ];
+    Umask = "0077";
   };
 
   services.ddns-updater = {
     enable = true;
     environment = {
-      SERVER_ENABLED="yes";
+      SERVER_ENABLED="no";
       CONFIG_FILEPATH = config.age.secrets.ddns-updater-config-json.path;
       PERIOD = "5m";
-      LOG_LEVEL = "debug";
+      LOG_LEVEL = "info";
     };
   };
 
