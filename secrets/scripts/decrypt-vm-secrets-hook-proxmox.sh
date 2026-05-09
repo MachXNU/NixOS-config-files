d70f7a3c3cd6814680c5c88078048c086abf689c
MachXNU <42501418+MachXNU@users.noreply.github.com>
2026-05-09 13:49:18 +0200

    Preserve secrets directory, even if empty

--- secrets/scripts/decrypt-vm-secrets-hook-proxmox.sh
+++ secrets/scripts/decrypt-vm-secrets-hook-proxmox.sh
@@ -8,6 +8,6 @@ case "$PHASE" in
     /root/NixOS-config-files/secrets/scripts/decrypt-vm-secrets.sh "$VMID"
     ;;
   post-stop)
-    rm -rf "/run/vm-secrets/$VMID"
+    rm -rf "/run/vm-secrets/$VMID/*"
     ;;
 esac
