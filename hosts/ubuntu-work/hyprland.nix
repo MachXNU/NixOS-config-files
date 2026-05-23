76c8045ffd94c5ee1e24541ef58cb9ba92069af8
Jb <42501418+MachXNU@users.noreply.github.com>
2026-05-23 20:24:12 +0000

    2nd monitor at home

--- hosts/ubuntu-work/hyprland.nix
+++ hosts/ubuntu-work/hyprland.nix
@@ -1,5 +1,8 @@
 {
-  monitor = ",1920x1080@60,auto,1,bitdepth,10";
+  monitor = [
+    ",1920x1080@60,0x0,1,bitdepth,10"
+    "desc:BNQ BenQ EX2710Q C1R00852019,2560x1440@144,1920x-1080,1,bitdepth,10"
+  ];
 
   env = [
     "LIBVA_DRIVER_NAME,nvidia"
