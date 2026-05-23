be7a1532f2f53f9e0895aa98f625d2b025243634
Jb <42501418+MachXNU@users.noreply.github.com>
2026-05-23 10:56:12 +0000

    Hyprlock layout on Ubuntu host

--- hosts/ubuntu-work/hyprlock-layout.nix
+++ hosts/ubuntu-work/hyprlock-layout.nix
@@ -1,38 +1,41 @@
 {
   hour = {
-    position = "-8%, 23%";
+    position = "-8%, 26%";
     halign = "center";
     valign = "center";
+    font_size = 180;
   };
 
   minute = {
     position = "0, 9%";
     halign = "center";
     valign = "center";
+    font_size = 180;
   };
 
   date = {
-    position = "2%, 1%";
+    position = "2%, -1%";
     halign = "center";
     valign = "center";
+    font_size = 20;
   };
 
   user = {
-    font_size = 25;
+    font_size = 20;
     position = "0, -38%";
     halign = "center";
     valign = "center";
   };
 
   image = {
-    size = 130;
+    size = 100;
     position = "0, -30%";
     halign = "center";
     valign = "center";
   };
 
   input = {
-    size = "420, 65";
+    size = "300, 55";
     position = "0, -43%";
     halign = "center";
     valign = "center";
