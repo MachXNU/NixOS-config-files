e104ef0e51384ce598546c16adb8661a675fadc5
Jb <42501418+MachXNU@users.noreply.github.com>
2026-05-26 15:23:55 +0200

    [Hyprland] Office monitors

--- hosts/ubuntu-work/hyprland.nix
+++ hosts/ubuntu-work/hyprland.nix
@@ -2,6 +2,8 @@
   monitor = [
     ",1920x1080@60,0x0,1,bitdepth,10"
     "desc:BNQ BenQ EX2710Q C1R00852019,2560x1440@144,1920x-1080,1,bitdepth,10"
+    "desc:Philips Consumer Electronics Company PHL 243V7 UHB1720022390,1920x1080@60,1920x0,1,bitdepth,8"
+    "desc:Philips Consumer Electronics Company PHL 243V7 UHB1720023672,1920x1080@60,3840x0,1,bitdepth,8"
   ];
 
   env = [
