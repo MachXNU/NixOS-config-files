6c8dab307a867b03cf89483d05c8f975771a2ad8
Jb <42501418+MachXNU@users.noreply.github.com>
2026-06-01 13:44:03 +0200

    Flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hyperland on NixOS with Noctalia";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
