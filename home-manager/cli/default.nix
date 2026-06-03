904b0bee910ece5462e5a9fbe652a002be131935
Jb <42501418+MachXNU@users.noreply.github.com>
2026-06-03 09:35:48 +0200

    [Work] [with reformating] Added smbclient

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -1,37 +1,40 @@
 {
   pkgs,
-  config,
   isWork,
   ...
-}: {
-  imports =
-    [
-      ./home-manager.nix
-      ./git.nix
-      ./neovim.nix
-      ./nvf.nix
-      ./ranger.nix
-      ./zsh.nix
-    ]
-    ++ (
-      if !isWork
-      then [
+}:
+{
+  imports = [
+    ./home-manager.nix
+    ./git.nix
+    ./neovim.nix
+    ./nvf.nix
+    ./ranger.nix
+    ./zsh.nix
+  ]
+  ++ (
+    if !isWork then
+      [
         ./ssh.nix
       ]
-      else []
-    );
+    else
+      [ ]
+  );
 
-  home.packages = with pkgs; [
-    file
-    tree
-    htop
-    efibootmgr
-    jq
-    fastfetch
-    jq
-    zip
-    unzip
-  ];
+  home.packages =
+    with pkgs;
+    [
+      file
+      tree
+      htop
+      efibootmgr
+      jq
+      fastfetch
+      jq
+      zip
+      unzip
+    ]
+    ++ (if isWork then [ samba ] else [ ]);
 
   home.shellAliases = {
     svim = "sudo -E ${pkgs.vim}/bin/vim";
