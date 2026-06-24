1e8575a3828701c6027f3ae89e416f61451cbe7e
Jb <42501418+MachXNU@users.noreply.github.com>
2026-06-24 11:26:00 +0200

    [Hyprland] Lua not working

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
-
+let
+  generatedLua = pkgs.replaceVars ./lua/init.lua {
+    dbusCmd = "${pkgs.dbus}/bin/dbus-update-activation-environment";
+    waypaper = "${pkgs.waypaper}/bin/waypaper";
+  };
+in
 {
   home.packages = with pkgs; [
     awww
@@ -7,16 +12,15 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+
+    configType = "lua";
+
     systemd.enable = true;
+
+    extraConfig = builtins.readFile generatedLua;
   };
 
   imports = [
-    ./binds.nix
-    ./decoration.nix
-    ./general.nix
-    ./animations.nix
-    ./misc.nix
-    ./inputs.nix
     ./screensharing.nix
   ];
 }
