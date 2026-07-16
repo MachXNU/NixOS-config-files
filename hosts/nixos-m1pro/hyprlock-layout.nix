4e9c9f7714334b21c867be1cea3adb4fb55e462c
Jb <42501418+MachXNU@users.noreply.github.com>
2026-07-16 18:58:46 +0200

    [Hyprlock] Adapted for the MacBook Pro's screen

--- hosts/nixos-m1pro/hyprlock-layout.nix
+++ hosts/nixos-m1pro/hyprlock-layout.nix
@@ -1,41 +1,41 @@
 {
   hour = {
-    position = "-8%, 23%";
+    position = "-8%, 25%";
     halign = "center";
     valign = "center";
-    font_size = 200;
+    font_size = 280;
   };
 
   minute = {
-    position = "0, 9%";
+    position = "0, 10%";
     halign = "center";
     valign = "center";
-    font_size = 200;
+    font_size = 280;
   };
 
   date = {
     position = "2%, 1%";
     halign = "center";
     valign = "center";
-    font_size = 25;
+    font_size = 33;
   };
 
   user = {
-    font_size = 25;
+    font_size = 33;
     position = "0, -38%";
     halign = "center";
     valign = "center";
   };
 
   image = {
-    size = 130;
+    size = 180;
     position = "0, -30%";
     halign = "center";
     valign = "center";
   };
 
   input = {
-    size = "420, 65";
+    size = "480, 75";
     position = "0, -43%";
     halign = "center";
     valign = "center";
