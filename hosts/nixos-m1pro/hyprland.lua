ae9dec1456911178058a52440499b8062db38c86
Jb <42501418+MachXNU@users.noreply.github.com>
2026-07-16 18:59:29 +0200

    [Hyprland] machine-specific patches

--- hosts/nixos-m1pro/hyprland.lua
+++ hosts/nixos-m1pro/hyprland.lua
@@ -1,6 +1,3 @@
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("GBM_BACKEND", "nvidia-drm")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("WLR_NO_HARDWARE_CURSORS", "1")
 
-hl.monitor({ output = "", mode = "2560x1440@144", bitdepth = 10 })
+hl.monitor({ output = "eDP-1", scale=2, bitdepth = 10 })
