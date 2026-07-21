b63aa836d86f3dcdf4ca180b0fcd4520d000c257
Jb <42501418+MachXNU@users.noreply.github.com>
2026-07-21 19:51:41 +0200

    [Noctalia] Brightness slider

--- home-manager/desktop/noctalia.nix
+++ home-manager/desktop/noctalia.nix
@@ -125,34 +125,33 @@
             { id = "KeepAwake"; }
             { id = "NightLight"; }
           ];
-          cards = [
-            {
-              enabled = true;
-              id = "profile-card";
-            }
-            {
-              enabled = true;
-              id = "shortcuts-card";
-            }
-            {
-              enabled = true;
-              id = "audio-card";
-            }
-            {
-              enabled = true;
-              id = "brightness-card";
-            }
-            {
-              enabled = true;
-              id = "weather-card";
-            }
-            {
-              enabled = true;
-              id = "media-sysmon-card";
-            }
-          ];
         };
-
+        cards = [
+          {
+            enabled = true;
+            id = "profile-card";
+          }
+          {
+            enabled = true;
+            id = "shortcuts-card";
+          }
+          {
+            enabled = true;
+            id = "audio-card";
+          }
+          {
+            enabled = true;
+            id = "brightness-card";
+          }
+          {
+            enabled = true;
+            id = "weather-card";
+          }
+          {
+            enabled = true;
+            id = "media-sysmon-card";
+          }
+        ];
       };
       dock = {
         enabled = false;
