0a9f5eaeed2a963d916316dbf659ffd3faf15521
Jb <42501418+MachXNU@users.noreply.github.com>
2026-08-10 21:33:40 +0200

    [nix-darwin] user home directory

--- config/darwin-configuration.nix
+++ config/darwin-configuration.nix
@@ -9,12 +9,12 @@
   ];
 
   users.users.${username} = {
-    home = "/Users/jb";
+    home = "/Users/${username}";
   };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 6;
+  system.stateVersion = 7;
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
